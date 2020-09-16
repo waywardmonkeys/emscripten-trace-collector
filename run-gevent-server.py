550cb9040f671b2a8aabc24f9b69acc36cbc9b74
feserr <feserr3@gmail.com>
2020-09-16 17:34:13 +0100

    Drop Python 2 support.

--- run-gevent-server.py
+++ run-gevent-server.py
@@ -1,21 +1,13 @@
 #! /usr/bin/env python
 
 from trace_collector import app
-import sys
+from wsgiref.simple_server import make_server
 
-if (sys.version_info > (3, 0)):
-    from wsgiref.simple_server import make_server
+with make_server('', 5000, app) as httpd:
+    print("Serving HTTP on port 5000...")
 
-    with make_server('', 5000, app) as httpd:
-        print("Serving HTTP on port 5000...")
+    # Respond to requests until process is killed
+    httpd.serve_forever()
 
-        # Respond to requests until process is killed
-        httpd.serve_forever()
-
-        # Alternative: serve one request, then exit
-        httpd.handle_request()
-else:
-    from gevent.wsgi import WSGIServer
-
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
+    # Alternative: serve one request, then exit
+    httpd.handle_request()
